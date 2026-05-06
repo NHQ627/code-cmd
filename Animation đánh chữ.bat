e5784d89a0f1023f1e3df0359ae678091f290a84
NHQ627 <nhq627@gmail.com>
2026-05-06 14:03:53 +0700

    Update Animation đánh chữ.bat

--- Animation đánh chữ.bat
+++ Animation đánh chữ.bat
@@ -7,7 +7,7 @@ echo %Esc%[?25l
 ((set "text=text") & (set "vitrivb=2,1")) & call :vanban
 ((set "text=arrrrrrrrrrrrrrrrr") & (set "vitrivb=4,2")) & call :vanban
 ((set "text=1+1=3") & (set "vitrivb=5,3")) & call :vanban
-((set "text=animation đánh chữ") & (set "vitrivb=7,5")) & call :vanban
+((set "text=animation đánh chữ") & (set "vitrivb=10,5")) & call :vanban
 
 
 pause >nul
@@ -15,13 +15,15 @@ exit
 
 :vanban
 SetLocal EnableDelayedExpansion
-set /a "X=!vitrivb:~2,1!","Y=!vitrivb:~0,1!"
-for /L %%i in (0,1,800) do (
-  set "char=!text:~%%i,1!"
-  if "!char!"=="" goto :eof
-  echo !Esc![!Y!;!X!H!char!
-  set /a "X+=1"
-  For /L %%d in (1 1 50)Do Call :Delay 2> nul
+for /F "Tokens=1-2 Delims=," %%A in ("%vitrivb%") Do (
+ Set "Y=%%A" & Set "X=%%B"
+ for /L %%i in (0,1,800) do (
+   set "char=!text:~%%i,1!"
+   if "!char!"=="" goto :eof
+   echo !Esc![!Y!;!X!H!char!
+   set /a "X+=1"
+   For /L %%d in (1 1 50)Do Call :Delay 2> nul
+ )
 )
 endlocal
 rem cre:NHQ627
